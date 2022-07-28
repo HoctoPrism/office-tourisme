a2b4f9fba081c88bf698b10f7e0a1a005f218285
unknown <nemexxix@gmail.com>
2022-07-28 12:51:01 +0200

    LARAVEL - crud Event DONE

--- laravel/app/Http/Controllers/API/EventController.php
+++ laravel/app/Http/Controllers/API/EventController.php
@@ -3,9 +3,100 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Event;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Validation\ValidationException;
 
 class EventController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return JsonResponse
+     */
+    public function index(): JsonResponse
+    {
+        $events = DB::table('events')
+            ->get()
+            ->toArray();
+
+        return response()->json([
+            'status' => 'Success',
+            'data' => $events
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     * @return JsonResponse
+     * @throws ValidationException
+     */
+    public function store(Request $request): JsonResponse
+    {
+        $this->validate($request ,[
+            'name' => 'required|max:100',
+        ]);
+
+        $event = Event::create([
+            'name' => $request->name,
+        ]);
+
+        return response()->json([
+            'status' => 'Success',
+            'data' => $event,
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param Event $event
+     * @return JsonResponse
+     */
+    public function show(Event $event): JsonResponse
+    {
+        return response()->json($event);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param Event $event
+     * @return JsonResponse
+     * @throws ValidationException
+     */
+    public function update(Request $request, Event $event): JsonResponse
+    {
+        $this->validate($request, [
+            'name' => 'required|max:100'
+        ]);
+
+        $event->update([
+            'name' => $request->name
+        ]);
+
+        return response()->json([
+            'status' => 'Mise à jour avec success'
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param Event $event
+     * @return JsonResponse
+     */
+    public function destroy(Event $event): JsonResponse
+    {
+        $event->delete();
+
+        return response()->json([
+            'status' => 'Supprimer avec success'
+        ]);
+    }
 }
--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\EventController;
 use App\Http\Controllers\API\TypeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -20,3 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::apiResource("types", TypeController::class);
+Route::apiResource("events", EventController::class);
