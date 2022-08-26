b47fe3d6db0e1a276844f384c680735b8af23301
unknown <nemexxix@gmail.com>
2022-08-26 23:46:17 +0200

    LARAVEL : auth terminée

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\AddressController;
+use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\EventController;
 use App\Http\Controllers\API\PlaceController;
 use App\Http\Controllers\API\ScheduleController;
@@ -19,12 +20,50 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::controller(AuthController::class)->group(function () {
+    Route::post('login', 'login');
+    Route::post('register', 'register');
+    Route::post('logout', 'logout');
+    Route::post('refresh', 'refresh');
+    Route::get('current-user', 'currentUser');
 });
 
-Route::apiResource("types", TypeController::class);
-Route::apiResource("events", EventController::class);
-Route::apiResource("schedules", ScheduleController::class);
-Route::apiResource("addresses", AddressController::class);
-Route::apiResource("places", PlaceController::class);
+Route::controller(TypeController::class)->group(function () {
+    Route::get('types', 'index');
+    Route::get('types/{type}', 'show');
+    Route::post('types', 'store')->middleware('auth:api');
+    Route::patch('types/{type}', 'update')->middleware('auth:api');
+    Route::delete('types/{type}', 'destroy')->middleware('auth:api');
+});
+
+Route::controller(ScheduleController::class)->group(function () {
+    Route::get('schedules', 'index');
+    Route::get('schedules/{schedule}', 'show');
+    Route::post('schedules', 'store')->middleware('auth:api');
+    Route::patch('schedules/{schedule}', 'update')->middleware('auth:api');
+    Route::delete('schedules/{schedule}', 'destroy')->middleware('auth:api');
+});
+
+Route::controller(EventController::class)->group(function () {
+    Route::get('events', 'index');
+    Route::get('events/{event}', 'show');
+    Route::post('events', 'store')->middleware('auth:api');
+    Route::patch('events/{event}', 'update')->middleware('auth:api');
+    Route::delete('events/{event}', 'destroy')->middleware('auth:api');
+});
+
+Route::controller(AddressController::class)->group(function () {
+    Route::get('addresses', 'index');
+    Route::get('addresses/{address}', 'show');
+    Route::post('addresses', 'store')->middleware('auth:api');
+    Route::patch('addresses/{address}', 'update')->middleware('auth:api');
+    Route::delete('addresses/{address}', 'destroy')->middleware('auth:api');
+});
+
+Route::controller(PlaceController::class)->group(function () {
+    Route::get('places', 'index');
+    Route::get('places/{place}', 'show');
+    Route::post('places', 'store')->middleware('auth:api');
+    Route::patch('places/{place}', 'update')->middleware('auth:api');
+    Route::delete('places/{place}', 'destroy')->middleware('auth:api');
+});
