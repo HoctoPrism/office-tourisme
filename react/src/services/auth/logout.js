1bbe655f3812ba3effb8bb3a04dd6cd8dcdb16a9
unknown <nemexxix@gmail.com>
2022-08-27 01:33:40 +0200

    REACT : logout DONE // redirection todo

--- react/src/services/auth/logout.js
+++ react/src/services/auth/logout.js
@@ -1,8 +1,11 @@
+import {useDispatch} from "react-redux";
+import {loggedFalse} from "../../component/features/loginButton/loginButtonSlice";
+
 function Logout () {
-  const dispatch = useDispatch()
-  dispatch(loggedFalse())
-  localStorage.removeItem('access_token');
-  return true
+    const dispatch = useDispatch()
+    dispatch(loggedFalse())
+    localStorage.removeItem('access_token');
+    return true
 }
 
-export default {Logout};
+export default Logout;
